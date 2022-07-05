25f8842f795f30d8b813343523b78a5724e00a59
Alidzhon <khosabekovalidzhon@gmail.com>
2022-07-05 19:26:39 +0300

    refactor: js file

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
-window.addEventListener('DOMContentLoaded', () => {
-      const menu = document.querySelector('.header__nav');
-      const menuItem = document.querySelectorAll('.header__item');
-      const hamburger = document.querySelector('.header__hamburger');
-      const buttonClose = document.querySelector('.header__button-close');
-      const body = document.querySelector('body');
-
-
-      hamburger.addEventListener('click', () => {
-            menu.classList.add('header__nav-active');
-      });
-      buttonClose.addEventListener('click', () => {
-            menu.classList.remove('header__nav-active');    
-      });
-      menuItem.forEach(item => {
-           item.addEventListener('click', () => {
-                menu.classList.remove('header__nav-active');    
-           });
-      });
-
-      body.addEventListener('click', (event) => {
-           const target = event.target;
-           target.addEventListener('click', () => {
-                menu.classList.remove('header__nav-active');  
-           });       
-      });
+window.addEventListener("DOMContentLoaded", () => {
+     const menu = document.querySelector(".header__nav");
+     const menuItem = document.querySelectorAll(".header__item");
+     const hamburger = document.querySelector(".header__hamburger");
+     const buttonClose = document.querySelector(".header__button-close");
+     const body = document.querySelector("body");
+     
+     function outsideClick(event) {
+     const target = event.target;
+     
+     if (target.closest(".header__nav") === null) {
+         menu.classList.remove("header__nav-active");
+     
+         body.removeEventListener("click", outsideClick);
+     
+     }}
+     
+     function closeMenu() {
+         menu.classList.remove("header__nav-active");
+         body.removeEventListener("click", outsideClick);
+     
+     }
+     
+     function openMenu(event) {
+        event.stopPropagation();
+        menu.classList.add("header__nav-active");
+        body.addEventListener("click", outsideClick);
+     }
+     
+     hamburger.addEventListener("click", openMenu);
+     
+     buttonClose.addEventListener("click", closeMenu);
+     
+     menuItem.forEach((item) => {
+     
+     item.addEventListener("click", closeMenu);
+     
+    });
+       
 });
